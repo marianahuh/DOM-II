4e9e77a48b1f27eb97b09f9d2cbb234fac172f92
Mariana <mhuh0176@gmail.com>
2019-02-02 11:34:27 -0800

    added preventDefault

--- js/index.js
+++ js/index.js
@@ -3,25 +3,33 @@ const logoEl = document.querySelector('.logo-heading');
 const h2El = document.querySelectorAll('h2');
 const signUpBtn = document.querySelectorAll('.btn');
 const imgs = document.querySelectorAll('img');
+const homeTag = document.querySelector('#home-tag');
+
+// Prevent Default
+homeTag.addEventListener('click', e => {
+  e.preventDefault();
+  prompt("What's the magic word?");
+});
 
 // Click Event
 logoEl.addEventListener('click', () => {
   TweenMax.from('.logo-heading', 2, { x: 300, ease: Elastic.easeOut });
   TweenMax.from('.nav-link', 1, { opacity: 0, y: 30, scale: 2, delay: 0.5, ease: Elastic.easeOut });
+  console.log('clicked logo');
 });
 
 // Mouse Enter Event
 Array.from(h2El).forEach(function (h2) {
   h2.addEventListener('mouseenter', e => {
     e.target.style.color = 'red';
-  })
+  });
 });
 
 //Mouse Leave Event
 Array.from(h2El).forEach(function (h2) {
   h2.addEventListener('mouseleave', e => {
     e.target.style.color = 'black';
-  })
+  });
 });
 
 // Double Click Event
@@ -29,12 +37,14 @@ Array.from(signUpBtn).forEach(function (button) {
   button.addEventListener('dblclick', e => {
     e.target.parentNode.style.color = '#16A2B8';
   });
+  console.log('double clicked');
 });
 
 Array.from(signUpBtn).forEach(function (button) {
   button.addEventListener('mouseleave', e => {
     e.target.parentNode.style.color = 'black';
   });
+  console.log('mouse left');
 });
 
 
@@ -46,6 +56,7 @@ window.addEventListener('scroll', () => {
   if (scrollEnd === scrollDown) {
     alert('Get On The Fun Bus!');
   }
+  console.log('scrolled');
 });
 
 
